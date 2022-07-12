a41510283a2910a671bd76d755917152de42b1c7
William <s3864077@rmit.edu.vn>
2022-07-12 16:39:13 +0700

    Initial Commit

---
+++ assignment 1/assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_1App.swift
+//  assignment 1
+//
+//  Created by William on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
